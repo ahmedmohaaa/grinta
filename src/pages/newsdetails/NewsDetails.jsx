b59c3fe420baf40cbea5889346f3d5ce1820e879
ahmedmohaaa <ma2012924@gmail.com>
2026-06-19 15:53:59 +0300

    Update NewsDetails.jsx

--- src/pages/newsdetails/NewsDetails.jsx
+++ src/pages/newsdetails/NewsDetails.jsx
@@ -5,7 +5,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
 import './NewsDetail.css';
 
-const API_BASE_URL = 'http://161.97.76.160/api';
+const API_BASE_URL = 'https://api.algrinta.com/api';
 
 const NewsDetail = () => {
   const { id } = useParams();
