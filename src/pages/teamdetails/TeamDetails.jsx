9e8d90dccafe79e05b22f3e083a874be9fc18d36
ahmedmohaaa <ma2012924@gmail.com>
2026-06-19 15:55:25 +0300

    Update API base URL to use HTTPS

--- src/pages/teamdetails/TeamDetails.jsx
+++ src/pages/teamdetails/TeamDetails.jsx
@@ -5,7 +5,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
 import './TeamDetails.css';
 
-const API_BASE_URL = 'http://161.97.76.160/api';
+const API_BASE_URL = 'https://api.algrinta.com/api';
 
 const TeamDetails = () => {
   const { id } = useParams(); // /team/33
